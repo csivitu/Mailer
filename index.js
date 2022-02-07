6aa6078efe5616e5ae3a60a2bda7990b0c44c5cc
GauriPradeep <gauripradeep21@gmail.com>
2022-02-08 02:26:42 +0530

    Added logic for sending emails

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const fs = require("fs");
 const dotenv = require("dotenv");
+const mailgunJS = require("mailgun-js");
 
 const emailList = "./test.txt";
 const htmlTemplate = "./template.html";
+const senderEmail = "outreach@csivit.com";
+const emailSubject = "CSI CCS Recruitments for Freshers '22";
 
 dotenv.config();
+
+const mailgun = mailgunJS({ apiKey: process.env.API_KEY, domain: process.env.DOMAIN });
 
 function getEmails(fileName) {
     const emails = fs.readFileSync(fileName).toString().split('\n');
     return emails;
-} 
+}
 
 function getHTMLTemplate(htmlTemplate) {
     const html = fs.readFileSync(htmlTemplate).toString();
     return html;
 }
 
-getEmails(emailList);
-getHTMLTemplate(htmlTemplate);
+function sendMail(senderEmail, receiverEmail, emailSubject, htmlTemplate) {
+    const data = {
+        from: senderEmail,
+        to: receiverEmail,
+        subject: emailSubject,
+        html: htmlTemplate,
+    };
 
+    mailgun.messages().send(data, (error, body) => {
+        if (error) console.log(error)
+        else console.log(body);
+    });
+}
+
+function sendEmails() {
+    const emails = getEmails(emailList);
+    const html = getHTMLTemplate(htmlTemplate);
+    emails.forEach((email) => {
+        sendMail(senderEmail, email, emailSubject, html);
+    })
+
+}
+
+sendEmails();
